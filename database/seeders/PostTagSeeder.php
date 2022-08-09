0593c3e350d79b4bc1b327b86536e74fcb7893a5
Nattapong <nattapong.pim@ku.th>
2022-08-09 11:12:58 +0700

    first commit

---
+++ database/seeders/PostTagSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use App\Models\Post;
+
+class PostTagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $posts = Post::all();
+        $tags = Tag::all();
+        foreach ($posts as $post) {
+            $post->tags()->sync($tags->random(rand(1, 3)));
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::where('email', 'admin@example.com')->first();
+        if (!$admin) {
+            $admin = new User();
+            $admin->name = 'Administrator N.';
+            $admin->role = 'ADMIN';
+            $admin->email = 'admin@example.com';
+            $admin->password = Hash::make('adminpass');
+            $admin->save();
+        }
+
+        $user = User::where('email', 'user01@example.com')->first();
+        if (!$user) {
+            $user = new User();
+            $user->name = 'User 01';
+            $user->role = 'USER';
+            $user->email = 'user01@example.com';
+            $user->password = Hash::make('userpass');
+            $user->save();
+    }
+}
